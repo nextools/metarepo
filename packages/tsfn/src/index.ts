d2aad04f8d34337c5a1ff624b0124b7a2832369b
psxcode <psxcode@gmail.com>
2019-04-11 12:13:12 +0200

    🐞 tsfn: fix isObject type guard

--- packages/tsfn/src/index.ts
+++ packages/tsfn/src/index.ts
@@ -16,7 +16,7 @@ export const UNDEFINED = void 0
 export const isUndefined = (value: any): value is undefined => value === UNDEFINED
 export const isNumber = (value: any): value is number => typeof value === 'number' && isFinite(value)
 export const isFunction = (value: any): value is Function => typeof value === 'function'
-export const isObject = (value: any): value is {} => Object.prototype.toString.call(value) === '[object Object]'
+export const isObject = (value: any): value is TAnyObject => Object.prototype.toString.call(value) === '[object Object]'
 
 export const requestAnimationFrame = (global as any as Window).requestAnimationFrame || global.setImmediate
 export const cancelAnimationFrame = (global as any as Window).cancelAnimationFrame || global.clearImmediate
