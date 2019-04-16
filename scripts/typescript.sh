9792a3fe4486d97acc21d113a6b7efb3ff8ce38c
Kir Belevich <kir.belevich@klarna.com>
2019-04-16 12:21:16 +0200

    🛠 cleanup

--- scripts/typescript.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if git apply --check --ignore-space-change --ignore-whitespace --directory node_modules/typescript/ typescript.patch 2> /dev/null; then
-  git apply --verbose --ignore-space-change --ignore-whitespace --directory node_modules/typescript/ typescript.patch
-fi
