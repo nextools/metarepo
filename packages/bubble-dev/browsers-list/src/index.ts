07e9a83d8c2f04b5fc39200491e79600d604ac83
Kir Belevich <kir.belevich@klarna.com>
2019-05-16 13:53:45 +0200

    🐣 bubble-dev/browsers-list: init

---
+++ packages/bubble-dev/browsers-list/src/index.ts
@@ -0,0 +1,16 @@
+// https://browserl.ist/?q=last+2+Chrome+versions%2C+last+2+Firefox+versions%2C+Firefox+ESR%2C+Safari+%3E%3D+10%2C+last+2+Edge+versions%2C+IE+11%2C+Last+2+Opera+versions%2C+iOS+%3E%3D+11.3%2C+last+2+ChromeAndroid+versions%2C+Android+%3E+4.4%2C+last+2+UCAndroid+versions%2C+last+2+Samsung+versions
+
+export const browsersList: string[] = [
+  'last 2 Chrome versions',
+  'last 2 Firefox versions',
+  'Firefox ESR',
+  'Safari >= 10',
+  'last 2 Edge versions',
+  'IE 11',
+  'Last 2 Opera versions',
+  'iOS >= 10',
+  'last 2 ChromeAndroid versions',
+  'Android > 4.4',
+  'last 2 UCAndroid versions',
+  'last 2 Samsung versions',
+]
