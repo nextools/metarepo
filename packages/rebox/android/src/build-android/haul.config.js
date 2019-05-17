1db9cbd5f3c70426f1cafcbb528b1da4a610515d
psxcode <psxcode@gmail.com>
2019-05-17 16:42:06 +0200

    🌱 rebox/android: add `buildAndroid` (close #13)

---
+++ packages/rebox/android/src/build-android/haul.config.js
@@ -0,0 +1,85 @@
+import path from 'path'
+import { createWebpackConfig } from 'haul'
+
+export default {
+  webpack: (env) => {
+    const config = createWebpackConfig({
+      entry: require.resolve('./App.js'),
+    })(env)
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      __REBOX_ENTRY_POINT__: path.resolve(process.env.REBOX_ENTRY_POINT),
+    }
+
+    config.module.rules = [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve('node_modules/react-native/'),
+          path.resolve('node_modules/metro/'),
+          path.resolve('node_modules/react-devtools-core/'),
+          path.resolve('node_modules/react-hot-loader/'),
+          path.resolve('node_modules/haul/node_modules/react-hot-loader/'),
+        ],
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              babelrc: false,
+              presets: [
+                require.resolve('metro-react-native-babel-preset'),
+              ],
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: path.resolve('node_modules/'),
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              babelrc: false,
+              presets: [
+                require.resolve('metro-react-native-babel-preset'),
+                require.resolve('@babel/preset-typescript'),
+              ],
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+      ...config.module.rules,
+    ]
+
+    config.performance = {
+      ...config.performance,
+      hints: false,
+    }
+
+    config.plugins = config.plugins.filter((plugin) => {
+      if (!plugin.constructor) {
+        return true
+      }
+
+      return plugin.constructor.name !== 'SourceMapDevToolPlugin'
+    })
+
+    config.resolve.extensions = [
+      ...config.resolve.extensions,
+      `.${env.platform}.js`,
+      `.${env.platform}.ts`,
+      `.${env.platform}.tsx`,
+      '.native.js',
+      '.native.ts',
+      '.native.tsx',
+      '.ts',
+      '.tsx',
+    ]
+
+    return config
+  },
+}
---
+++ packages/rebox/android/src/build-android/index.ts
@@ -0,0 +1,90 @@
+import path from 'path'
+import { promisify } from 'util'
+import { writeFile } from 'fs'
+import execa from 'execa'
+import moveFile from 'move-file'
+
+const pWriteFile = promisify(writeFile)
+
+export type TBuildAndroidOptions = {
+  entryPointPath: string,
+  appName: string,
+  outputPath: string,
+}
+
+export const buildAndroid = async (options: TBuildAndroidOptions) => {
+  const androidPath = path.join('node_modules', '@rebox', 'android', 'android')
+
+  await execa(
+    'haul',
+    [
+      'bundle',
+      '--config',
+      require.resolve('./haul.config.js'),
+      '--platform',
+      'android',
+      '--dev',
+      'false',
+      '--minify',
+      'true',
+      '--progress',
+      'none',
+      '--bundle-output',
+      path.join(androidPath, 'app', 'src', 'main', 'assets', 'index.android.bundle'),
+    ],
+    {
+      stderr: process.stderr,
+      env: {
+        FORCE_COLOR: '1',
+        REBOX_ENTRY_POINT: options.entryPointPath,
+      },
+    }
+  )
+
+  await execa(
+    path.resolve(androidPath, 'gradlew'),
+    [
+      'clean',
+      '--console=plain',
+      '--quiet',
+      '--no-daemon',
+      '--warning-mode=none',
+    ],
+    {
+      cwd: androidPath,
+      stderr: process.stderr,
+      env: {
+        FORCE_COLOR: '1',
+      },
+    }
+  )
+
+  const stringsXmlPath = path.join(androidPath, 'app', 'src', 'main', 'res', 'values', 'strings.xml')
+
+  await pWriteFile(stringsXmlPath, `<resources><string name="app_name">${options.appName}</string></resources>`)
+
+  await execa(
+    path.resolve(androidPath, 'gradlew'),
+    [
+      'assemble',
+      '--console=plain',
+      '--quiet',
+      '--no-daemon',
+      '--warning-mode=none',
+    ],
+    {
+      cwd: androidPath,
+      stderr: process.stderr,
+      env: {
+        FORCE_COLOR: '1',
+      },
+    }
+  )
+
+  await pWriteFile(stringsXmlPath, '<resources><string name="app_name">rebox</string></resources>')
+
+  const originalApkPath = path.join(androidPath, 'app', 'build', 'outputs', 'apk', 'debug', 'app-debug.apk')
+  const newApkPath = path.join(options.outputPath, `${options.appName}.apk`)
+
+  await moveFile(originalApkPath, newApkPath)
+}
