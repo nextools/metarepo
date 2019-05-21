1cc1a2a1638c39f2d55294773fdaab2ab5ea1166
Kir Belevich <kir.belevich@klarna.com>
2019-05-21 16:33:36 +0200

    🐣 stili: init

---
+++ packages/stili/src/index.native.ts
@@ -0,0 +1,19 @@
+import { TextStyle } from 'react-native' // eslint-disable-line
+import { TExtend, getObjectKeys, isUndefined } from 'tsfn'
+
+export type TStyle = TExtend<TextStyle, {
+  fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900,
+}>
+
+type TCssProps = TextStyle
+
+export const normalizeStyle = (style: TStyle): TCssProps =>
+  getObjectKeys(style).reduce((result, key) => {
+    const value = style[key]
+
+    if (key === 'fontWeight' && !isUndefined(value)) {
+      result.fontWeight = String(value) as TCssProps['fontWeight']
+    }
+
+    return result
+  }, {} as TCssProps)
---
+++ packages/stili/src/index.web.ts
@@ -0,0 +1,40 @@
+import { CSSProperties } from 'react' // eslint-disable-line
+import { getObjectKeys, TExtend } from 'tsfn'
+
+export type TStyle = TExtend<CSSProperties, {
+  fontSmoothing?: string,
+  tapHighlightColor?: string,
+  fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900,
+}>
+
+type TCssProps = TExtend<CSSProperties, {
+  WebkitFontSmoothing?: string,
+}>
+
+export const normalizeStyle = (style: TStyle): TCssProps =>
+  getObjectKeys(style).reduce((result, key) => {
+    const value = style[key]
+
+    if (key === 'userSelect') {
+      result.WebkitUserSelect = value as TCssProps['WebkitUserSelect']
+      result.MozUserSelect = value as TCssProps['MozUserSelect']
+      result.msUserSelect = value as TCssProps['msUserSelect']
+      result.userSelect = value as TCssProps['userSelect']
+    }
+
+    if (key === 'tapHighlightColor') {
+      result.WebkitTapHighlightColor = value as TCssProps['WebkitTapHighlightColor']
+    }
+
+    if (key === 'fontSmoothing') {
+      result.WebkitFontSmoothing = value as TCssProps['WebkitFontSmoothing']
+    }
+
+    if (key === 'appearance') {
+      result.WebkitAppearance = value as TCssProps['WebkitAppearance']
+      result.MozAppearance = value as TCssProps['MozAppearance']
+      result.appearance = value as TCssProps['appearance']
+    }
+
+    return result
+  }, {} as TCssProps)
