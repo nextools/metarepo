a3280eba419948f523bc5fb6b045cccfc901011c
psxcode <psxcode@gmail.com>
2019-05-29 14:16:10 +0200

    🐞 tsfn: add more `is` functions

--- packages/tsfn/src/index.ts
+++ packages/tsfn/src/index.ts
@@ -14,9 +14,14 @@ export const EMPTY_ARRAY = [] as any
 export const UNDEFINED = void 0
 
 export const isUndefined = (value: any): value is undefined => value === UNDEFINED
+export const isNull = (value: any): value is null => value === null
+export const isBoolean = (value: any): value is boolean => typeof value === 'boolean'
 export const isNumber = (value: any): value is number => typeof value === 'number' && isFinite(value)
+export const isString = (value: any): value is string => typeof value === 'string'
 export const isFunction = (value: any): value is Function => typeof value === 'function'
+export const isArray = (value: any): value is any[] => Array.isArray(value)
 export const isObject = (value: any): value is TAnyObject => Object.prototype.toString.call(value) === '[object Object]'
+export const isSymbol = (value: any): value is symbol => typeof value === 'symbol'
 
 export const requestAnimationFrame = (global as any as Window).requestAnimationFrame || global.setImmediate
 export const cancelAnimationFrame = (global as any as Window).cancelAnimationFrame || global.clearImmediate
