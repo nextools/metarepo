9574441c75a7e90589dd61b5dd82f4b34cc2d55a
Alex Feinstein <psxcode@gmail.com>
2019-06-13 09:37:42 +0200

    🐞 refun: fix `mapDefaultProps` issue with explicit `undefined` (close #24)

--- packages/refun/src/map-default-props.ts
+++ packages/refun/src/map-default-props.ts
@@ -1,2 +1,14 @@
+import { getObjectKeys, isUndefined } from 'tsfn'
+
 export const mapDefaultProps = <T extends {}> (defaultProps: T) =>
-  <P extends {}> (props: P): P & T => ({ ...defaultProps, ...props })
+  <P extends {}> (props: P): P & T => {
+    const mergedProps = { ...defaultProps, ...props }
+
+    return getObjectKeys(props).reduce((result, key) => {
+      if (isUndefined(result[key]) && Reflect.has(defaultProps, key)) {
+        result[key] = (defaultProps as typeof mergedProps)[key]
+      }
+
+      return result
+    }, mergedProps)
+  }
