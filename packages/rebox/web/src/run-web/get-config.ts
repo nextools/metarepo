a2266759526f307045c11179bb45934f341b8584
Moria <moria.am@gmail.com>
2019-07-23 16:48:20 +0200

    🐞 rebox/web: fix images and videos loader

--- packages/rebox/web/src/run-web/get-config.ts
+++ packages/rebox/web/src/run-web/get-config.ts
@@ -65,12 +65,12 @@ export const getConfig = (entryPointPath: string, htmlTemplatePath: string): TWe
       {
         test: /\.png$/,
         exclude: /node_modules/,
-        loader: 'images/[name].[hash].[ext]',
+        loader: 'file-loader?images/[name].[hash].[ext]',
       },
       {
         test: /\.mp4$/,
         exclude: /node_modules/,
-        loader: 'videos/[name].[hash].[ext]',
+        loader: 'file-loader?videos/[name].[hash].[ext]',
       },
     ],
   },
