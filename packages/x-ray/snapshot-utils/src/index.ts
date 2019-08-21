738268c0e419560ebf6b28107bbecda03d24db95
alex.feinstein <alex.feinstein@klarna.com>
2019-08-21 16:44:41 +0200

    🛠 x-ray/snapshot-utils: remove package

--- packages/x-ray/snapshot-utils/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as checkSnapshot } from './check-snapshot'
-export * from './types'
--- packages/x-ray/snapshot-utils/src/types.ts
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react' // eslint-disable-line
-
-export type TMeta = {
-  options: {
-    name: string,
-  },
-  element: ReactElement<any>,
-}
