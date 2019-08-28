352bd0db27e95786c175f4c35cfed4a572ba264a
Kir Belevich <kir@belevi.ch>
2019-08-28 15:42:29 +0200

    🛠 cleanup

--- packages/primitives/button/x-ray/screenshots.tsx
+++ packages/primitives/button/x-ray/screenshots.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { mapPropsIterable } from 'autoprops'
 import { TMeta } from '@x-ray/screenshot-utils'
 import { serializeElement } from '@x-ray/common-utils'
-import { Button } from '../src/index.web'
+import { Button } from '../src'
 import * as metaFile from '../meta'
 
 export default mapPropsIterable(metaFile, ({ id, props }): TMeta => ({
