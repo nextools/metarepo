aed16d7d4906c0d03987e7a2aeee088b44e4b178
Kir Belevich <kir@belevi.ch>
2019-08-30 10:49:01 +0200

    🐞 bubble-dev/start-preset: refactor using latest babel/preset-env

--- packages/bubble-dev/start-preset/src/cli/index.js
+++ packages/bubble-dev/start-preset/src/cli/index.js
@@ -19,8 +19,6 @@ require('@babel/register')({
     require.resolve('@babel/preset-react'),
   ],
   plugins: [
-    require.resolve('@babel/plugin-syntax-dynamic-import'),
-    require.resolve('babel-plugin-dynamic-import-node'),
     require.resolve('@babel/plugin-syntax-bigint'),
   ],
   extensions: [
