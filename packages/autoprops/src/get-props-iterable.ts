8c2858846b26b45ecbdbf6441f16d70c47446f82
alex.feinstein <alex.feinstein@klarna.com>
2019-09-04 13:42:19 +0200

    🐞 autoprops: enhance `serializeProps`

--- packages/autoprops/src/get-props-iterable.ts
+++ packages/autoprops/src/get-props-iterable.ts
@@ -5,34 +5,35 @@ import { map } from 'iterama'
 import { getPropsImpl } from './get-props'
 import { getNextPermImpl } from './get-next-perm'
 import { TMetaFile } from './types'
-import { SerializeProps } from './serialize-props'
+import { serializeProps } from './serialize-props'
 import { isChildrenMap } from './is-children-map'
 import { createChildren } from './create-children'
 
-export const getPropsIterable = <T>(metaFile: TMetaFile<T>) => ({
-  int: BigInt.zero as BigInteger | null,
-  *[Symbol.iterator]() {
-    const serialize = SerializeProps()
+export const getPropsIterable = <T>(metaFile: TMetaFile<T>): Iterable<{ id: string, props: T }> => {
+  let int: BigInteger | null = BigInt.zero
 
-    while (this.int !== null) {
-      const props = getPropsImpl(this.int, metaFile)
-      const sha = new jssha('SHA-1', 'TEXT')
+  return {
+    *[Symbol.iterator]() {
+      while (int !== null) {
+        const props = getPropsImpl(int, metaFile)
+        const sha = new jssha('SHA-1', 'TEXT')
 
-      sha.update(serialize(props))
+        sha.update(serializeProps(int, metaFile))
 
-      if (isDefined(metaFile.childrenConfig) && isChildrenMap(props.children)) {
-        props.children = createChildren(metaFile.childrenConfig, props.children)
+        if (isDefined(metaFile.childrenConfig) && isChildrenMap(props.children)) {
+          props.children = createChildren(metaFile.childrenConfig, props.children)
+        }
+
+        yield {
+          id: sha.getHash('B64'),
+          props: props as T,
+        }
+
+        int = getNextPermImpl(int, metaFile)
       }
-
-      yield {
-        id: sha.getHash('B64'),
-        props: props as T,
-      }
-
-      this.int = getNextPermImpl(this.int, metaFile)
-    }
-  },
-})
+    },
+  }
+}
 
 export const mapPropsIterable = <T, R>(metaFile: TMetaFile<T>, xf: (value: { id: string, props: T }) => R): Iterable<R> => {
   return map(xf)(getPropsIterable(metaFile))
--- packages/autoprops/src/serialize-props.ts
+++ packages/autoprops/src/serialize-props.ts
@@ -1,38 +1,96 @@
+/* eslint-disable no-use-before-define */
 import { isValidElement } from 'react'
-import { isFunction, isSymbol, isUndefined, isRegExp, TAnyObject } from 'tsfn'
+import { isFunction, isSymbol, isUndefined, isRegExp, TAnyObject, isString, isDefined } from 'tsfn'
+import BigInt, { BigInteger } from 'big-integer'
 import { getElementName } from './get-element-name'
+import { TMetaFile } from './types'
+import { unpackPerm } from './unpack-perm'
+import { getIndexedName } from './get-indexed-name'
 
-export const SerializeProps = () => {
-  let functionIndex = -1
-  let symbolIndex = -1
-  let regexpIndex = -1
-  let elementIndex = -1
+const getValue = (valueIndex: number, values: any[], key: string, required?: string[]): string | undefined => {
+  let index = -1
 
-  return (obj: TAnyObject): string => JSON.stringify(obj, (key, value) => {
+  if (isDefined(required) && required.includes(key)) {
+    index = valueIndex
+  } else if (valueIndex > 0) {
+    index = valueIndex - 1
+  }
+
+  if (index >= 0) {
+    const value = values[index]
+
     if (isFunction(value)) {
-      functionIndex++
-
-      return value.name === '' ? `[function (${functionIndex})]` : `[function(${value.name}) (${functionIndex})]`
+      return value.name === ''
+        ? `[function (${index})]`
+        : `[function(${value.name}) (${index})]`
     }
 
     if (isSymbol(value)) {
-      symbolIndex++
-
-      return isUndefined(value.description) ? `[symbol (${symbolIndex})]` : `[symbol(${value.description}) (${symbolIndex})]`
+      return isUndefined(value.description)
+        ? `[symbol (${index})]`
+        : `[symbol(${value.description}) (${index})]`
     }
 
     if (isRegExp(value)) {
-      regexpIndex++
-
-      return `[regexp(${value.toString()}) (${regexpIndex})]`
+      return `[regexp(${value}) (${index})]`
     }
 
     if (isValidElement(value)) {
-      elementIndex++
-
-      return `[react(${getElementName(value)}) (${elementIndex})]`
+      return `[react(${getElementName(value)}) (${index})]`
     }
 
-    return value
-  })
+    return `${value}`
+  }
 }
+
+const getChildValue = (int: BigInteger, childMeta: TMetaFile, childKey: string, required?: string[]): any => {
+  if (isDefined(required) && required.includes(childKey)) {
+    return getPropsImpl(int, childMeta)
+  } else if (int.greater(BigInt.zero)) {
+    return getPropsImpl(int.minus(BigInt.one), childMeta)
+  }
+}
+
+const getPropsImpl = (int: BigInteger, metaFile: TMetaFile): TAnyObject => {
+  const propsKeys = Object.keys(metaFile.config.props)
+  const result: TAnyObject = {}
+  const { values } = unpackPerm(int, metaFile)
+
+  let i = 0
+
+  for (; i < propsKeys.length; ++i) {
+    const propKey = propsKeys[i]
+    const valueIndex = values[i].toJSNumber()
+    const value = getValue(valueIndex, metaFile.config.props[propKey], propKey, metaFile.config.required)
+
+    if (isString(value)) {
+      result[propKey] = value
+    }
+  }
+
+  if (isDefined(metaFile.childrenConfig)) {
+    const childrenMap: TAnyObject = {}
+    let hasChildren = false
+
+    for (; i < values.length; ++i) {
+      const childIndex = i - propsKeys.length
+      const childKey = metaFile.childrenConfig.children[childIndex]
+      const valueIndex = values[i]
+      const value = getChildValue(valueIndex, metaFile.childrenConfig.meta[childKey], childKey, metaFile.childrenConfig.required)
+
+      if (isDefined(value)) {
+        const childIndexedKey = getIndexedName(metaFile.childrenConfig.children, childIndex)
+        childrenMap[childIndexedKey] = value
+        hasChildren = true
+      }
+    }
+
+    if (hasChildren) {
+      result.children = childrenMap
+    }
+  }
+
+  return result
+}
+
+export const serializeProps = (int: BigInteger, metaFile: TMetaFile): string => JSON.stringify(getPropsImpl(int, metaFile))
