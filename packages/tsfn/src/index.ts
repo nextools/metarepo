89b694ac5499bdf55ac6542263fe31f1311ced4b
alex.feinstein <alex.feinstein@klarna.com>
2019-09-09 13:40:15 +0200

    🐞 tsfn: add `getObjectValues` and `getObjectEntries`

--- packages/tsfn/src/index.ts
+++ packages/tsfn/src/index.ts
@@ -18,6 +18,9 @@ export type TRequiredKeys<T extends {}> = Exclude<{
 }[keyof T], undefined>
 
 export const getObjectKeys = <T extends {}> (obj: T) => Object.keys(obj) as (keyof T)[]
+export const getObjectValues = <T extends {}> (obj: T): T[keyof T][] => Object.values(obj)
+export const getObjectEntries = <T extends {}> (obj: T) => Object.entries(obj) as unknown as [keyof T, T[keyof T]][]
+
 export const objectHas = <T extends {}, K extends keyof T> (obj: T, key: K): obj is T & { [k in K]-?: T[k] } => Reflect.has(obj, key)
 
 export const EMPTY_OBJECT = Object.freeze(Object.create(null)) as any
