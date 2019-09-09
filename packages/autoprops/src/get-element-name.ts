430e06d562295011338380b7fb7e929ed28db4b7
alex.feinstein <alex.feinstein@klarna.com>
2019-09-09 09:51:53 +0200

    🛠 autoprops: inline get-element-name

--- packages/autoprops/src/get-element-name.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement, FC } from 'react'
-
-export const getElementName = (element: ReactElement<any>) => {
-  if (typeof element.type === 'string') {
-    return element.type
-  }
-
-  return (element.type as FC<any>).displayName || element.type.name
-}
