d9c3268682f39906d92ab7f39c6957154d57613f
alex.feinstein <alex.feinstein@klarna.com>
2019-09-13 09:24:44 +0200

    💥 bubble-dev/start-preset: update react-native v0.60

--- packages/bubble-dev/start-preset/src/plugins/android-screenshots-plugin.ts
+++ packages/bubble-dev/start-preset/src/plugins/android-screenshots-plugin.ts
@@ -1,81 +1,66 @@
 import plugin, { StartFilesProps } from '@start/plugin'
 
-export default (appPath: string) =>
-  plugin<StartFilesProps, void>('x-ray-android-screenshots', ({ logMessage }) => async ({ files }) => {
-    if (files.length === 0) {
-      return logMessage('no files, skipping')
-    }
+export default (fontsDir?: string) => plugin<StartFilesProps, void>('x-ray-android-screenshots', ({ logMessage }) => async ({ files }) => {
+  if (files.length === 0) {
+    return logMessage('no files, skipping')
+  }
 
-    const path = await import('path')
-    const { rnResolve } = await import('rn-resolve')
-    const { broResolve } = await import('bro-resolve')
-    const { runEmulator, serveJsBundle, installApp, launchApp } = await import('@rebox/android')
-    const { run: runWeb } = await import('@rebox/web')
-    const { runScreenshotsServer, prepareFiles } = await import('@x-ray/native-screenshots')
-    const { runServer: runUiServer } = await import('@x-ray/screenshot-utils')
+  const path = await import('path')
+  const { rnResolve } = await import('rn-resolve')
+  const { broResolve } = await import('bro-resolve')
+  const { run: runAndroid } = await import('@rebox/android')
+  const { run: runWeb } = await import('@rebox/web')
+  const { runScreenshotsServer, prepareFiles } = await import('@x-ray/native-screenshots')
+  const { runServer: runUiServer } = await import('@x-ray/screenshot-utils')
 
-    const entryPointPath = await rnResolve('@x-ray/native-screenshots-app')
+  const entryPointPath = await rnResolve('@x-ray/native-screenshots-app')
 
-    await prepareFiles(entryPointPath, files.map((file) => file.path))
+  await prepareFiles(entryPointPath, files.map((file) => file.path))
 
-    let killServer = null
-    let killEmulator = null
+  const runScreenshots = await runScreenshotsServer({ platform: 'android', dpr: 3 })
 
-    try {
-      killServer = await serveJsBundle({
+  let killAll = null
+
+  try {
+    killAll = await runAndroid({
+      appName: 'X-Ray',
+      appId: 'org.bubble_dev.xray',
+      entryPointPath,
+      fontsDir,
+      dependencyNames: [
+        'react-native-svg',
+        'react-native-view-shot',
+      ],
+      portsToForward: [3002],
+      isHeadless: true,
+    })
+
+    const { result, resultData, hasBeenChanged } = await runScreenshots()
+
+    killAll()
+
+    if (hasBeenChanged) {
+      const entryPointPath = await broResolve('@x-ray/ui')
+      const htmlTemplatePath = path.join(path.dirname(entryPointPath), 'index.html')
+
+      const closeReboxServer = await runWeb({
         entryPointPath,
-        isDev: false,
+        htmlTemplatePath,
+        isQuiet: true,
       })
 
-      logMessage('server is ready')
+      console.log('open http://localhost:3000/ to approve or discard changes')
 
-      killEmulator = await runEmulator({
-        isHeadless: true,
-        portsToForward: [3002, 8081],
+      await runUiServer({
+        platform: 'android',
+        result,
+        resultData,
       })
-
-      logMessage('device is ready')
-
-      const runScreenshots = await runScreenshotsServer({ platform: 'android', dpr: 3 })
-
-      await installApp({ appPath })
-
-      logMessage('app is installed')
-
-      await launchApp({ appId: 'org.bubble_dev.xray' })
-
-      logMessage('app is launched')
-
-      const { result, resultData, hasBeenChanged } = await runScreenshots()
-
-      killEmulator()
-
-      if (hasBeenChanged) {
-        const entryPointPath = await broResolve('@x-ray/ui')
-        const htmlTemplatePath = path.join(path.dirname(entryPointPath), 'index.html')
-
-        const closeReboxServer = await runWeb({
-          entryPointPath,
-          htmlTemplatePath,
-          isQuiet: true,
-        })
-
-        console.log('open http://localhost:3000/ to approve or discard changes')
-
-        await runUiServer({
-          platform: 'android',
-          result,
-          resultData,
-        })
-        await closeReboxServer()
-      }
-    } finally {
-      if (killEmulator !== null) {
-        killEmulator()
-      }
-
-      if (killServer !== null) {
-        killServer()
-      }
+      await closeReboxServer()
     }
-  })
+  } finally {
+    if (killAll !== null) {
+      killAll()
+    }
+  }
+})
