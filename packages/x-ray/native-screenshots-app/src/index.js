b45157450a0c7ec6369646bbe1f4a9d5ad713686
alex.feinstein <alex.feinstein@klarna.com>
2019-09-18 16:13:54 +0200

    🐞 x-ray/native-screenshots-app: handle `SizeContext`

--- packages/x-ray/native-screenshots-app/src/index.js
+++ packages/x-ray/native-screenshots-app/src/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import ViewShot from 'react-native-view-shot' // eslint-disable-line
+import { SizeContext } from '@primitives/size'
 import files from './files' // eslint-disable-line
 
 const defaultStyles = {}
 const hasOwnWidthStyles = {
   flexDirection: 'row',
   alignItems: 'flex-start',
+}
+
+const SizeProvider = ({ shouldWaitForResize, onCapture, children }) => {
+  if (shouldWaitForResize) {
+    let numMountedSizes = 0
+    let numUpdatedSizes = 0
+
+    return (
+      <SizeContext.Provider value={{
+        onSizeMount: () => {
+          numMountedSizes++
+        },
+        onSizeUpdate: () => {
+          numUpdatedSizes++
+
+          if (numUpdatedSizes === numMountedSizes) {
+            onCapture()
+          }
+        },
+      }}
+      >
+        {children}
+      </SizeContext.Provider>
+    )
+  }
+
+  return children
 }
 
 export class App extends Component {
@@ -37,6 +65,9 @@ class Main extends Component {
       path: null,
     }
 
+    this.isCapturing = false
+    this.viewShot = null
+    this.onRef = this.onRef.bind(this)
     this.onCapture = this.onCapture.bind(this)
   }
 
@@ -48,54 +79,76 @@ class Main extends Component {
       path,
       item: iterator.next().value,
       iterator,
-    }))
+    }), async () => {
+
+    })
   }
 
-  async onCapture(data) {
-    const res = await fetch('http://localhost:3002/upload', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        data,
-        id: this.state.item.id,
-        serializedElement: this.state.item.serializedElement,
-        path: this.state.path,
-      }),
-      keepalive: true,
-    })
+  onRef(ref) {
+    this.viewShot = ref
 
-    if (!res.ok) {
+    const { item } = this.state
+
+    if (item !== null && !item.options.shouldWaitForResize) {
+      this.onCapture()
+    }
+  }
+
+  async onCapture() {
+    if (this.viewShot === null || this.isCapturing) {
       return
     }
 
-    const nextResult = this.state.iterator.next()
+    this.isCapturing = true
 
-    if (!nextResult.done) {
-      this.setState({
-        item: nextResult.value,
+    try {
+      const data = await this.viewShot.capture()
+
+      this.isCapturing = false
+
+      const res = await fetch('http://localhost:3002/upload', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data,
+          id: this.state.item.id,
+          serializedElement: this.state.item.serializedElement,
+          path: this.state.path,
+        }),
+        keepalive: true,
       })
-    } else if (this.state.fileIndex < files.length - 1) {
-      const nextFileIndex = this.state.fileIndex + 1
-      const { path, content } = files[nextFileIndex]
-      const iterator = content[Symbol.iterator]()
 
-      this.setState(() => ({
-        item: iterator.next().value,
-        fileIndex: nextFileIndex,
-        path,
-        iterator,
-      }))
-    } else {
+      if (!res.ok) {
+        return
+      }
+
+      const nextResult = this.state.iterator.next()
+
+      if (!nextResult.done) {
+        this.setState({
+          item: nextResult.value,
+        })
+      } else if (this.state.fileIndex < files.length - 1) {
+        const nextFileIndex = this.state.fileIndex + 1
+        const { path, content } = files[nextFileIndex]
+        const iterator = content[Symbol.iterator]()
+
+        this.setState(() => ({
+          item: iterator.next().value,
+          fileIndex: nextFileIndex,
+          path,
+          iterator,
+        }))
+      } else {
       // finish
-      await fetch('http://localhost:3002/done')
+        await fetch('http://localhost:3002/done')
+      }
+    } catch (err) {
+      console.log(err)
     }
-  }
-
-  onCaptureFailure(err) {
-    console.log(err)
   }
 
   render() {
@@ -106,26 +159,31 @@ class Main extends Component {
     }
 
     return (
-      <View style={item.options.hasOwnWidth ? hasOwnWidthStyles : defaultStyles}>
-        <ViewShot
-          captureMode="mount"
-          options={{ result: 'base64' }}
+      <SizeProvider
+        shouldWaitForResize={item.options.shouldWaitForResize}
+        onCapture={this.onCapture}
+      >
+        <View
+          style={item.options.hasOwnWidth ? hasOwnWidthStyles : defaultStyles}
           key={`${fileIndex}:${item.id}`}
-          style={{
-            padding: item.options.overflow,
-            paddingTop: item.options.overflowTop,
-            paddingBottom: item.options.overflowBottom,
-            paddingLeft: item.options.overflowLeft,
-            paddingRight: item.options.overflowRight,
-            maxWidth: item.options.maxWidth,
-            backgroundColor: item.options.backgroundColor || '#fff',
-          }}
-          onCapture={this.onCapture}
-          onCaptureFailure={this.onCaptureFailure}
         >
-          {item.element}
-        </ViewShot>
-      </View>
+          <ViewShot
+            ref={this.onRef}
+            options={{ result: 'base64' }}
+            style={{
+              padding: item.options.overflow,
+              paddingTop: item.options.overflowTop,
+              paddingBottom: item.options.overflowBottom,
+              paddingLeft: item.options.overflowLeft,
+              paddingRight: item.options.overflowRight,
+              maxWidth: item.options.maxWidth,
+              backgroundColor: item.options.backgroundColor || '#fff',
+            }}
+          >
+            {item.element}
+          </ViewShot>
+        </View>
+      </SizeProvider>
     )
   }
 }
