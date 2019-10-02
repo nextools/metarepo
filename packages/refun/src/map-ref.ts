da5e274d302dd9bc3cfdc19c2da289ecdcf91f6e
alex.feinstein <alex.feinstein@klarna.com>
2019-10-02 09:47:58 +0200

    🐞 refun: fix `mapRef` type

--- packages/refun/src/map-ref.ts
+++ packages/refun/src/map-ref.ts
@@ -1,12 +1,12 @@
 import { useRef, MutableRefObject } from 'react'
 import { TExtend } from 'tsfn'
 
-export const mapRef = <N extends string, T> (name: N, initialValue: T) => <P extends {}> (props: P) => {
+export const mapRef = <N extends string, T> (name: N, initialValue: T) => <P extends {}> (props: P): TExtend<P, { [K in N]: MutableRefObject<T> }> => {
   const ref = useRef(initialValue)
 
   return {
     ...props,
     [name]: ref,
     // FIXME: https://github.com/Microsoft/TypeScript/issues/13948
-  } as any as TExtend<P, { [K in N]: MutableRefObject<T> }>
+  } as any
 }
