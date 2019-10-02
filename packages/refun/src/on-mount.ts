0d4b9b047610177f6214e98af3ccaa586a4f2f4b
alex.feinstein <alex.feinstein@klarna.com>
2019-10-02 11:32:30 +0200

    🐞 refun: optimize `onMount`, allow async functions

--- packages/refun/src/on-mount.ts
+++ packages/refun/src/on-mount.ts
@@ -1,7 +1,20 @@
+import { useRef } from 'react'
+import { EMPTY_ARRAY, EMPTY_OBJECT, NOOP } from 'tsfn'
 import { useEffectFn } from './utils'
 
-export const onMount = <P extends {}> (onMount: (props: P) => void | (() => void)) => (props: P): P => {
-  useEffectFn(() => onMount(props), [])
+export const onMount = <P extends {}> (onMountFn: (props: P) => Promise<void> | void) => (props: P): P => {
+  const propsRef = useRef<P>(EMPTY_OBJECT)
+  const onMountRef = useRef<() => void>(NOOP)
+
+  if (onMountRef.current === NOOP) {
+    onMountRef.current = () => {
+      onMountFn(propsRef.current)
+    }
+  }
+
+  propsRef.current = props
+
+  useEffectFn(onMountRef.current, EMPTY_ARRAY)
 
   return props
 }
