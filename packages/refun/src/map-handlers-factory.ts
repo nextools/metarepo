bcb82f63a9ec68f716bf82398d700fd727f07dbd
alex.feinstein <alex.feinstein@klarna.com>
2019-10-03 14:58:59 +0200

    🛠 refun: extract `mapHandlersFactory`

--- packages/refun/src/map-handlers-factory.ts
+++ packages/refun/src/map-handlers-factory.ts
@@ -1,21 +1,15 @@
 import { useRef } from 'react'
 import { getObjectKeys, TExtend, EMPTY_OBJECT } from 'tsfn'
+import { THandlers, TActualHandlers } from './map-handlers'
 
-export type TActualHandlers <T> = {
-  [key in keyof T]: (...args: any[]) => void
-}
-
-export type THandlers <P> = {
-  [key: string]: (props: P) => (...args: any[]) => void,
-}
-
-export const mapHandlers = <P extends {}, R extends THandlers<P>> (handlers: R) =>
+export const mapHandlersFactory = <P extends {}, R extends THandlers<P>> (getHandlers: (props: P) => R) =>
   (props: P): TExtend<P, TActualHandlers<R>> => {
     const actualHandlers = useRef<TActualHandlers<R>>(EMPTY_OBJECT)
     const propsRef = useRef<P>(props)
     propsRef.current = props
 
     if (actualHandlers.current === EMPTY_OBJECT) {
+      const handlers = getHandlers(props)
       actualHandlers.current = getObjectKeys(handlers).reduce((result, key) => {
         result[key] = (...args) => handlers[key](propsRef.current)(...args)
 
