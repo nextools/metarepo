b5c6d17302094457ddfffe67ffe488790809cdf3
alex.feinstein <alex.feinstein@klarna.com>
2019-10-03 17:12:50 +0200

    🐞 x-ray/ui: use new `onMount`

--- packages/x-ray/ui/src/components/Screenshot.tsx
+++ packages/x-ray/ui/src/components/Screenshot.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { startWithType, component, onMount, mapHandlers, mapState } from 'refun'
+import { startWithType, component, mapHandlers, mapState } from 'refun'
 import { apiLoadScreenshot, TApiLoadScreenshotOpts } from '../api'
 import { mapStoreDispatch } from '../store'
 import { TSize } from '../types'
 import { actionError } from '../actions'
+import { onMountAsync } from './on-mount-async'
 
 export type TScreenshot = TSize & TApiLoadScreenshotOpts
 
@@ -11,25 +12,17 @@ export const Screenshot = component(
   startWithType<TScreenshot>(),
   mapStoreDispatch,
   mapState('src', 'setSrc', () => null as string | null, []),
-  onMount(({ dispatch, setSrc, ...opts }) => {
-    let isMounted = true
+  onMountAsync(async ({ isMountedRef, dispatch, setSrc, ...opts }) => {
+    try {
+      const blob = await apiLoadScreenshot(opts)
+      const url = URL.createObjectURL(blob)
 
-    ;(async () => {
-      try {
-        const blob = await apiLoadScreenshot(opts)
-        const url = URL.createObjectURL(blob)
-
-        if (isMounted) {
-          setSrc(url)
-        }
-      } catch (err) {
-        console.log(err)
-        dispatch(actionError(err.message))
+      if (isMountedRef.current) {
+        setSrc(url)
       }
-    })()
-
-    return () => {
-      isMounted = false
+    } catch (err) {
+      console.log(err)
+      dispatch(actionError(err.message))
     }
   }),
   mapHandlers({
