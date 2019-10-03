a5059f74b418aafe9b3d6a64f6b2797b864a15bb
alex.feinstein <alex.feinstein@klarna.com>
2019-10-03 17:09:34 +0200

    🐞 refun: allow async function in `onChange`

--- packages/refun/src/on-change.ts
+++ packages/refun/src/on-change.ts
@@ -2,7 +2,7 @@ import { EMPTY_OBJECT } from 'tsfn'
 import { useRef } from 'react'
 import { shallowEqualByKeys } from './utils'
 
-export const onChange = <P extends {}>(getFn: (props: P) => void, watchKeys: (keyof P)[]) => (props: P): P => {
+export const onChange = <P extends {}>(getFn: (props: P) => Promise<void> | void, watchKeys: (keyof P)[]) => (props: P): P => {
   const prevProps = useRef<P>(EMPTY_OBJECT)
 
   if (!shallowEqualByKeys(prevProps.current, props, watchKeys)) {
