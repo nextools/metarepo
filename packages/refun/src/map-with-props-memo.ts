4fab4d51639915299ee5d92fd4e450766032900e
alex.feinstein <alex.feinstein@klarna.com>
2019-10-03 14:59:24 +0200

    🛠 refun: extract `mapWithPropsMemo`

---
+++ packages/refun/src/map-with-props-memo.ts
@@ -0,0 +1,18 @@
+import { useRef } from 'react'
+import { TExtend, EMPTY_OBJECT } from 'tsfn'
+import { shallowEqualByKeys } from './utils'
+
+export const mapWithPropsMemo = <P extends {}, R extends {}>(getFn: (props: P) => R, watchKeys: (keyof P)[]) => (props: P): TExtend<P, R> => {
+  const prevProps = useRef<P>(EMPTY_OBJECT)
+  const prevResult = useRef<R>(EMPTY_OBJECT)
+
+  if (prevResult.current === EMPTY_OBJECT || !shallowEqualByKeys(prevProps.current, props, watchKeys)) {
+    prevProps.current = props
+    prevResult.current = getFn(props)
+  }
+
+  return {
+    ...props,
+    ...prevResult.current,
+  }
+}
