e9f2aa3a98ff702aaef47439699dd15b6f76f09d
alex.feinstein <alex.feinstein@klarna.com>
2019-10-07 16:59:47 +0200

    🌱 spyt: add `createRafSpy`

--- packages/spyt/src/index.ts
+++ packages/spyt/src/index.ts
@@ -49,3 +49,53 @@ export const createTimeoutSpy = () => {
     },
   }
 }
+
+export const createRafSpy = () => {
+  let lastId = 0
+  let timers: {
+    id: number,
+    cb: () => void,
+    isCleared: boolean,
+  }[] = []
+  const clearedIds: {
+    id: number,
+  }[] = []
+
+  return {
+    getRequestAnimationFrameCalls() {
+      return new Array(timers.length).fill([])
+    },
+    getCancelAnimationFrameCalls() {
+      return clearedIds.map((clearedIds) => [clearedIds.id])
+    },
+    requestAnimationFrame(cb: () => void) {
+      const id = lastId++
+      timers.push({
+        id,
+        cb,
+        isCleared: false,
+      })
+
+      return id
+    },
+    cancelAnimationFrame(id: number) {
+      timers = timers.map((timer) => {
+        if (timer.id === id) {
+          timer.isCleared = true
+        }
+
+        return timer
+      })
+
+      clearedIds.push({ id })
+    },
+    tick() {
+      timers.forEach((timer) => {
+        if (timer.isCleared === false) {
+          timer.cb()
+        }
+        timer.isCleared = true
+      })
+    },
+  }
+}
