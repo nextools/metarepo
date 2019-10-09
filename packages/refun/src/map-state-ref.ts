183e6f16473b0d0f66b31d5ba72beccf1f0cb965
alex.feinstein <alex.feinstein@klarna.com>
2019-10-09 15:37:43 +0200

    🐞 refun: fix `mapStateRef` to rerender on flush

--- packages/refun/src/map-state-ref.ts
+++ packages/refun/src/map-state-ref.ts
@@ -1,25 +1,30 @@
 import { useState, useRef, MutableRefObject } from 'react'
-import { TExtend3, EMPTY_OBJECT, UNDEFINED } from 'tsfn'
+import { TExtend3, EMPTY_OBJECT, NOOP } from 'tsfn'
 import { shallowEqualByKeys } from './utils'
 
 export const mapStateRef = <SN extends string, SSN extends string, P extends {}, R> (stateName: SN, stateFlusherName: SSN, getValue: (props: P) => R, watchKeys: (keyof P)[]) =>
   (props: P): TExtend3<P, { [K in SN]: MutableRefObject<R> }, { [K in SSN]: () => void }> => {
-    const isFirstRender = useRef(true)
-    const useStateResult = useState(UNDEFINED)
-    const stateRef = useRef<R>(isFirstRender.current ? getValue(props) : EMPTY_OBJECT)
-    const prevProps = useRef<P>(props)
+    const useStateResult = useState<R>(EMPTY_OBJECT)
+    const prevProps = useRef<P>(EMPTY_OBJECT)
+    const stateRef = useRef<R>(EMPTY_OBJECT)
+    const stateFlushRef = useRef(NOOP)
 
-    isFirstRender.current = false
+    if (prevProps.current === EMPTY_OBJECT) {
+      stateFlushRef.current = () => {
+        useStateResult[1](stateRef.current)
+      }
+    }
 
-    if (!shallowEqualByKeys(prevProps.current, props, watchKeys)) {
-      prevProps.current = props
+    if (prevProps.current === EMPTY_OBJECT || !shallowEqualByKeys(prevProps.current, props, watchKeys)) {
       stateRef.current = getValue(props)
     }
+
+    prevProps.current = props
 
     return {
       ...props,
       [stateName]: stateRef,
-      [stateFlusherName]: useStateResult[1],
+      [stateFlusherName]: stateFlushRef.current,
       // FIXME: https://github.com/Microsoft/TypeScript/issues/13948
     } as any
   }
