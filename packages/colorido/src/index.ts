0837e90616437f4001e4f5f7e2c4184358e37023
Fernando Via Canel <fernando.via@gmail.com>
2019-12-12 15:36:25 +0100

    🌱 colorido: introduce isColor type guard

--- packages/colorido/src/index.ts
+++ packages/colorido/src/index.ts
@@ -1,3 +1,5 @@
 export type TColor = [number, number, number, number]
 
 export const colorToString = (color: TColor): string => `rgba(${color.join(', ')})`
+
+export const isColor = (value: any): value is TColor => Array.isArray(value) && value.length === 4
