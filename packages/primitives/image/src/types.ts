defcef0353434964073974b7120a1c5f564c16e0
irminakacz <irmina.kaczmarek@klarna.com>
2020-01-22 10:51:09 +0100

    🐞 primitives/image, themeables/image: move style related props from primitive to themeable for image (#206)
    * 🐞 primitives/image, themeables/image: move style related props from primitive to themeable for image
    
    * 🛠 make height and width optional

--- packages/primitives/image/src/types.ts
+++ packages/primitives/image/src/types.ts
@@ -4,12 +4,6 @@ export type TImage = {
   id?: string,
   alt?: string,
   source: string,
-  height: number,
-  width: number,
-  topLeftRadius?: number,
-  topRightRadius?: number,
-  bottomRightRadius?: number,
-  bottomLeftRadius?: number,
   onLoad?: () => void,
   onError?: () => void,
 } & TThemeableImage
--- packages/themeables/image/src/types.ts
+++ packages/themeables/image/src/types.ts
@@ -1,5 +1,11 @@
 export type TThemeableImage = {
   resizeMode?: 'contain' | 'cover',
+  height?: number,
+  width?: number,
+  topLeftRadius?: number,
+  topRightRadius?: number,
+  bottomRightRadius?: number,
+  bottomLeftRadius?: number,
 }
 
 export type TThemeImage<InputProps> = (props: InputProps) => TThemeableImage
