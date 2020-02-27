d7fefb047848cdc818d6d0ce5d44bb4486aacacf
alex.feinstein <alex.feinstein@klarna.com>
2020-02-27 10:50:52 +0100

    🐞 sandbox/ui: export components required for plugins

--- packages/sandbox/ui/src/index.web.ts
+++ packages/sandbox/ui/src/index.web.ts
@@ -1,2 +1,26 @@
 export * from './App'
-export { TComponents, TTheme } from './types'
+export { injectReducer } from './store'
+export * from './components/label'
+export * from './components/layout'
+export * from './components/size-background'
+export * from './components/size-block'
+export * from './components/size-border'
+export * from './components/size-button'
+export * from './components/size-checkbox'
+export * from './components/size-content'
+export * from './components/size-link'
+export * from './components/size-parent-block'
+export * from './components/size-select'
+export * from './components/size-text'
+export * from './components/size'
+export {
+  TState,
+  TAction,
+  TActionAsync,
+  TActionCreator,
+  TActionWithPayload,
+  TActionWithPayloadCreator,
+  TAnyAction,
+} from './store/types'
+export { TPlugin } from './components/plugin-provider'
+export { TComponents, TTheme, TThemeIcons, TPackageJson } from './types'
