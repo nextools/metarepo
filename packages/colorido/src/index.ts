9f3cb0851c0505930afef9bd743a43e8df80e321
Kir Belevich <kir@belevi.ch>
2020-03-05 15:05:45 +0100

    🐞 colorido: more robust `isColor`

--- packages/colorido/src/index.ts
+++ packages/colorido/src/index.ts
@@ -2,4 +2,8 @@ export type TColor = [number, number, number, number]
 
 export const colorToString = (color: TColor): string => `rgba(${color.join(', ')})`
 
-export const isColor = (value: any): value is TColor => Array.isArray(value) && value.length === 4
+export const isColor = (value: any): value is TColor => {
+  return Array.isArray(value) &&
+         value.length === 4 &&
+         value.every((item) => typeof item === 'number')
+}
