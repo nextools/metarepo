7c44fbf396e121b7365a6ea310d79f777bd4f59b
psxcode <psxcode@gmail.com>
2020-03-12 14:07:05 +0100

    🛠 auto/log: delete package

--- packages/auto/log/src/utils.ts
+++
@@ -1,5 +0,0 @@
-export const SLACK_HOOKS_URL = 'https://hooks.slack.com/services/'
-export const SLACK_MAX_ATTACHMENTS = 100
-export const TELEGRAM_API_URL = 'https://api.telegram.org/'
-export const TELEGRAM_MESSAGE_MAX_LENGTH = 4096
-export const GITHUB_API_REPOS_URL = 'https://api.github.com/repos/'
