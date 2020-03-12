04ca2b481fd2235ec46908ee23ad2a8ff2e945d3
psxcode <psxcode@gmail.com>
2020-03-12 14:08:51 +0100

    🛠 auto/utils: delete package

--- packages/auto/utils/src/make-regexp.ts
+++
@@ -1,7 +0,0 @@
-import escapeStringRegexp from 'escape-string-regexp'
-
-export const makeRegExp = (input: string) => {
-  const escapedInput = escapeStringRegexp(input).replace(/\\\*/g, '.*')
-
-  return new RegExp(escapedInput)
-}
