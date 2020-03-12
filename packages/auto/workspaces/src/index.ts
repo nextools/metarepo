a4c776e37977aa0a9290990cb984a7e2e59ad0fd
psxcode <psxcode@gmail.com>
2020-03-12 14:08:26 +0100

    🛠 auto/workspaces: delete package

--- packages/auto/workspaces/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { getCrossDependents } from './get-cross-dependents'
-export { getDependentsOf } from './get-dependents-of'
-export { getDependentsCount } from './get-dependents-count'
-export * from './types'
--- packages/auto/workspaces/src/types.ts
+++
@@ -1,9 +0,0 @@
-export type TDependent = {
-  name: string,
-  range: string | null,
-  devRange: string | null,
-}
-
-export type TCrossDependents = {
-  [name: string]: TDependent[],
-}
