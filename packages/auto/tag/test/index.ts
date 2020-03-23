a51d53d2393bed31ffcf6cc4c1c81ccc70deb105
psxcode <psxcode@gmail.com>
2020-03-23 13:57:39 +0100

    🐣 auto/tag: init

---
+++ packages/auto/tag/test/index.ts
@@ -0,0 +1,74 @@
+import test from 'blue-tape'
+import { mock } from 'mocku'
+import { createSpy, getSpyCalls } from 'spyfn'
+import { TPackageRelease } from '@auto/core'
+import { prefixes } from './prefixes'
+
+test('writePublishTags: multiple tags', async (t) => {
+  const execaSpy = createSpy(() => Promise.resolve())
+
+  const unmock = mock('../src', {
+    execa: { default: execaSpy },
+  })
+
+  const packages: TPackageRelease[] = [
+    {
+      name: 'ns/a',
+      type: 'patch',
+      version: '0.1.1',
+      dir: 'fakes/a',
+      json: {
+        name: '@ns/a',
+        version: '0.0.1',
+      },
+      deps: null,
+      devDeps: null,
+      messages: null,
+    },
+    {
+      name: 'b',
+      type: 'minor',
+      version: '0.2.0',
+      dir: 'fakes/b',
+      json: {
+        name: 'b',
+        version: '0.1.0',
+      },
+      deps: null,
+      devDeps: null,
+      messages: null,
+    },
+    {
+      name: 'c',
+      type: null,
+      version: null,
+      dir: 'fakes/c',
+      json: {
+        name: 'c',
+        version: '1.0.0',
+      },
+      deps: null,
+      devDeps: null,
+      messages: null,
+    },
+  ]
+
+  const { writePublishTags } = await import('../src')
+
+  await writePublishTags({
+    config: {},
+    prefixes,
+    packages,
+  })
+
+  t.deepEquals(
+    getSpyCalls(execaSpy).map((call) => call.slice(0, 2)),
+    [
+      ['git', ['tag', '-a', 'ns/a@0.1.1', '-m', 'ns/a@0.1.1']],
+      ['git', ['tag', '-a', 'b@0.2.0', '-m', 'b@0.2.0']],
+    ],
+    'multiple tags'
+  )
+
+  unmock()
+})
