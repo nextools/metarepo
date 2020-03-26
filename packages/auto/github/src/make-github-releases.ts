d0282ffab5465b55944da8e2204bdc2b0fb0236b
psxcode <psxcode@gmail.com>
2020-03-26 10:44:15 +0100

    🐞 auto/github: ignore initial dependecnies in log

--- packages/auto/github/src/make-github-releases.ts
+++ packages/auto/github/src/make-github-releases.ts
@@ -1,43 +1,58 @@
 import pAll from 'p-all'
 import fetch from 'node-fetch'
-import { THook, TPackageRelease, TRequiredPrefixes } from '@auto/core'
+import { THook, TMessage, TLogReleaseType } from '@auto/core'
 import { isString, TReadonly } from 'tsfn'
 import { GITHUB_API_REPOS_URL } from './constants'
-import { compileMessages } from './compile-messages'
 import { TGithubConfig } from './types'
-
-const makeGithubRelease = (config: TGithubConfig, prefixes: TRequiredPrefixes) => (pkg: TReadonly<TPackageRelease>) => async (): Promise<void> => {
-  if (pkg.version === null) {
-    return
-  }
-
-  await fetch(
-    `${GITHUB_API_REPOS_URL}${config.username}/${config.repo}/releases`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${config.token}`,
-        'User-Agent': 'auto-tools',
-      },
-      body: JSON.stringify({
-        tag_name: `${pkg.name}@${pkg.version}`,
-        name: `${pkg.name}@${pkg.version}`,
-        body: compileMessages(pkg, prefixes),
-      }),
-    }
-  )
-}
 
 export const makeGithubReleases = (githubConfig: TReadonly<TGithubConfig>): THook => async ({ packages, prefixes }) => {
   if (!isString(githubConfig.token)) {
     throw new Error('GitHub token is required')
   }
 
-  const maker = makeGithubRelease(githubConfig, prefixes)
-
   await pAll(
-    packages.map(maker),
+    packages.map((pkg) => async (): Promise<void> => {
+      if (pkg.version === null) {
+        return
+      }
+
+      const compileMessages = (): string => {
+        let result = (pkg.messages || []) as TMessage<TLogReleaseType>[]
+
+        if (pkg.deps !== null && pkg.type !== 'initial') {
+          const depNames = Object.keys(pkg.deps)
+            .filter((name) => Boolean(packages.find((pkg) => pkg.name === name)?.type !== 'initial'))
+
+          if (depNames.length > 0) {
+            result = result.concat({
+              type: 'dependencies',
+              message: `update dependencies \`${depNames.join('`, `')}\``,
+            })
+          }
+        }
+
+        return result
+          .map((message) => `* ${prefixes[message.type]} ${message.message}`)
+          .join('\n')
+      }
+
+      await fetch(
+        `${GITHUB_API_REPOS_URL}${githubConfig.username}/${githubConfig.repo}/releases`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `token ${githubConfig.token}`,
+            'User-Agent': 'auto-tools',
+          },
+          body: JSON.stringify({
+            tag_name: `${pkg.name}@${pkg.version}`,
+            name: `${pkg.name}@${pkg.version}`,
+            body: compileMessages(),
+          }),
+        }
+      )
+    }),
     { concurrency: 2 }
   )
 }
