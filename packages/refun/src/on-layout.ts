d48709d202e7619e741689064b4ff6c4a97ca4a4
psxcode <psxcode@gmail.com>
2020-03-27 17:04:32 +0100

    🌱 refun: add `onLayout` and `onLayoutRef`

--- packages/refun/src/on-layout.ts
+++ packages/refun/src/on-layout.ts
@@ -1,27 +1,26 @@
-import { useRef, useLayoutEffect, Ref } from 'react'
-import { EMPTY_OBJECT, TExtend, NOOP } from 'tsfn'
+import { useRef, useLayoutEffect } from 'react'
+import { EMPTY_OBJECT, NOOP } from 'tsfn'
+import { shallowEqualByKeys } from './utils'
 
-export const onLayout = <P extends {}, RN extends string, REF> (refName: RN, onLayoutHandler: (ref: REF, props: P) => void) =>
-  (props: P): TExtend<P, { [k in RN]: Ref<REF> }> => {
-    const ref = useRef<REF>(null)
+export const onLayout = <P extends {}> (onLayoutHandler: (props: P) => Promise<void> | void, watchKeys: (keyof P)[]) =>
+  (props: P): P => {
+    const shouldCallHandler = useRef(true)
     const propsRef = useRef<P>(EMPTY_OBJECT)
     const useEffectFnRef = useRef(NOOP)
 
+    shouldCallHandler.current = propsRef.current === EMPTY_OBJECT || !shallowEqualByKeys(propsRef.current, props, watchKeys)
     propsRef.current = props
 
     if (useEffectFnRef.current === NOOP) {
       useEffectFnRef.current = () => {
-        if (ref.current !== null) {
-          onLayoutHandler(ref.current, propsRef.current)
+        if (shouldCallHandler.current) {
+          // eslint-disable-next-line @typescript-eslint/no-floating-promises
+          onLayoutHandler(propsRef.current)
         }
       }
     }
 
     useLayoutEffect(useEffectFnRef.current)
 
-    // FIXME https://github.com/microsoft/TypeScript/issues/13948
-    return {
-      ...props,
-      [refName]: ref,
-    } as any
+    return props
   }
