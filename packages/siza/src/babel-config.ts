a3b5759e2743d88fb84b6fd5be839c5257f3e638
Kir Belevich <kir@belevi.ch>
2020-04-03 09:52:47 +0200

    🐞 siza: use built-in Babel config

---
+++ packages/siza/src/babel-config.ts
@@ -0,0 +1,22 @@
+export const babelConfig = {
+  babelrc: false,
+  inputSourceMap: null,
+  sourceMaps: false,
+  compact: false,
+  presets: [
+    [
+      require.resolve('@babel/preset-env'),
+      {
+        targets: { browsers: ['last 1 Chrome version'] },
+        modules: false,
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
+    require.resolve('@babel/preset-typescript'),
+    require.resolve('@babel/preset-react'),
+  ],
+  plugins: [
+    require.resolve('@babel/plugin-transform-runtime'),
+  ],
+}
