cc925c230be25415456892befe2da897048588c7
Kir Belevich <kir@belevi.ch>
2020-04-09 10:52:12 +0200

    💥 iterama: `reduce` to a value instead of an iterable

--- packages/iterama/src/reduce.ts
+++ packages/iterama/src/reduce.ts
@@ -1,15 +1,13 @@
-export type ReducerFn<T, R> = (acc: R, val: T, index: number) => R
+export type TReduceFn<T, R> = (acc: R, value: T, index: number) => R
 
-export const reduce = <T, R> (reducer: ReducerFn<T, R>, initial: R) => (iterable: Iterable<T>): Iterable<R> => ({
-  *[Symbol.iterator]() {
-    let state = initial
-    let i = 0
+export const reduce = <T, R>(reduceFn: TReduceFn<T, R>, initial: R) => (iterable: Iterable<T>): R => {
+  let state = initial
+  let i = 0
 
-    for (const value of iterable) {
-      state = reducer(state, value, i++)
-    }
+  for (const value of iterable) {
+    state = reduceFn(state, value, i++)
+  }
 
-    yield state
-  },
-})
+  return state
+}
 
--- packages/iterama/test/reduce.ts
+++ packages/iterama/test/reduce.ts
@@ -1,75 +1,30 @@
 import test from 'tape'
 import { createSpy, getSpyCalls } from 'spyfn'
-import { pipe } from '@psxcode/compose'
-import { map } from '../src/map'
 import { reduce } from '../src/reduce'
+import { range } from '../src/range'
 
-export const add = (a: number, b: number) => a + b
+test('iterama: reduce', (t) => {
+  const iterable = range(5)
+  const add = (a: number, b: number): number => a + b
+  const spyReducer: typeof add = createSpy(({ args }) => add(args[0], args[1]))
+  const result = reduce(spyReducer, 0)(iterable)
 
-const gen = function *(n: number) {
-  for (let i = 0; i < n; ++i) {
-    yield i
-  }
-}
-const multBy = (x: number) => (val: number) => val * x
-const mult2 = multBy(2)
-
-test('iterama reduce: works with arrays', (t) => {
-  const data = [1, 2, 3, 4, 5]
-  const spy: typeof add = createSpy(({ args }) => add(...args))
-  const result = [...reduce(spy, 0)(data)]
-
-  t.deepEquals(result, [15])
   t.deepEquals(
-    getSpyCalls(spy),
-    [
-      [0, 1, 0],
-      [1, 2, 1],
-      [3, 3, 2],
-      [6, 4, 3],
-      [10, 5, 4],
-    ]
-  )
-
-  t.end()
-})
-
-test('iterama reduce: works chained', (t) => {
-  const data = [1, 2, 3, 4, 5]
-  const spy: typeof add = createSpy(({ args }) => add(...args))
-  const result = [...pipe(reduce(spy, 0), map(mult2))(data)]
-
-  t.deepEquals(result, [30])
-  t.deepEquals(
-    getSpyCalls(spy),
-    [
-      [0, 1, 0],
-      [1, 2, 1],
-      [3, 3, 2],
-      [6, 4, 3],
-      [10, 5, 4],
-    ]
-  )
-
-  t.end()
-})
-
-test('iterama reduce: works with Generators', (t) => {
-  const data = gen(6)
-  const spy: typeof add = createSpy(({ args }) => add(...args))
-  const result = [...reduce(spy, 0)(data)]
-
-  t.deepEquals(result, [15])
-  t.deepEquals(
-    getSpyCalls(spy),
+    getSpyCalls(spyReducer),
     [
       [0, 0, 0],
       [0, 1, 1],
       [1, 2, 2],
       [3, 3, 3],
       [6, 4, 4],
-      [10, 5, 5],
-    ]
+    ],
+    'should pass accumulator, value and counter to reducer function '
+  )
+
+  t.deepEquals(
+    result,
+    10,
+    'should iterate and reduce over iterable'
   )
 
   t.end()
