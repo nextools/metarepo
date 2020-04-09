faabe8c10cf232b508b90dd46fbd42335b9ee802
Kir Belevich <kir@belevi.ch>
2020-04-09 10:50:25 +0200

    💥 iterama: limit `length` to `Number.MAX_SAFE_INTEGER` always and by default

--- packages/iterama/src/length.ts
+++ packages/iterama/src/length.ts
@@ -1,8 +1,12 @@
-/* eslint-disable no-var, no-empty, block-scoped-var */
 import { iterate } from './iterate'
 
-export const length = (maxLength: number) => <T> (iterable: Iterable<T>): number => {
-  for (var i = 0, it = iterate(iterable); i < maxLength && !it.next().done; ++i) { }
+export const length = <T>(iterable: Iterable<T>): number => {
+  const iterator = iterate(iterable)
+  let i = 0
+
+  while (i < Number.MAX_SAFE_INTEGER && !iterator.next().done) {
+    i++
+  }
 
   return i
 }
--- packages/iterama/test/length.ts
+++ packages/iterama/test/length.ts
@@ -1,62 +1,16 @@
 import test from 'tape'
 import { length } from '../src/length'
+import { range } from '../src/range'
 
-const gen = function *(n: number) {
-  for (let i = 0; i < n; ++i) {
-    yield i
-  }
-}
+test('iterama: length', (t) => {
+  const iterable = range(5)
+  const result = length(iterable)
 
-test('iterama length: should work with arrays', (t) => {
-  const data = [1, 2, 3, 4, 5]
-  const result = length(Number.MAX_SAFE_INTEGER)(data)
-
-  t.equals(result, data.length)
-
-  t.end()
-})
-
-test('iterama length: should work with maxLength', (t) => {
-  const data = [1, 2, 3, 4, 5]
-  const result = length(2)(data)
-
-  t.equals(result, 2)
-
-  t.end()
-})
-
-test('iterama length: should work negative maxLength', (t) => {
-  const data = [1, 2, 3, 4, 5]
-  const result = length(-2)(data)
-
-  t.equals(result, 0)
-
-  t.end()
-})
-
-test('iterama length: should work maxLength === 0', (t) => {
-  const data = [1, 2, 3, 4, 5]
-  const result = length(0)(data)
-
-  t.equals(result, 0)
-
-  t.end()
-})
-
-test('iterama length: should work with Sets', (t) => {
-  const data = new Set([1, 2, 3, 4, 5])
-  const result = length(Number.MAX_SAFE_INTEGER)(data)
-
-  t.equals(result, 5)
-
-  t.end()
-})
-
-test('iterama length: should work with Generators', (t) => {
-  const data = gen(5)
-  const result = length(Number.MAX_SAFE_INTEGER)(data)
-
-  t.equals(result, 5)
+  t.equals(
+    result,
+    5,
+    'should return length of iterable'
+  )
 
   t.end()
 })
