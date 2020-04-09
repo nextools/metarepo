15afc0b491259a5c86239032ac732757bad987e7
Kir Belevich <kir@belevi.ch>
2020-04-09 11:00:59 +0200

    🌱 iterama: add `takeAsync` helper

---
+++ packages/iterama/test/take-async.ts
@@ -0,0 +1,61 @@
+import test from 'tape'
+import { pipe } from '@psxcode/compose'
+import { takeAsync } from '../src/take-async'
+import { rangeAsync } from '../src/range-async'
+import { toArrayAsync } from '../src/to-array-async'
+
+test('iterama: takeAsync + positive number', async (t) => {
+  const iterable = rangeAsync(5)
+  const result = await pipe(
+    takeAsync(3),
+    toArrayAsync
+  )(iterable)
+
+  t.deepEquals(
+    result,
+    [0, 1, 2],
+    'should take N first elements'
+  )
+})
+
+test('iterama: takeAsync + negative number', async (t) => {
+  const iterable = rangeAsync(5)
+  const result = await pipe(
+    takeAsync(-3),
+    toArrayAsync
+  )(iterable)
+
+  t.deepEquals(
+    result,
+    [2, 3, 4],
+    'should take N last elements'
+  )
+})
+
+test('iterama: takeAsync + more than needed', async (t) => {
+  const iterable = rangeAsync(5)
+  const result = await pipe(
+    takeAsync(42),
+    toArrayAsync
+  )(iterable)
+
+  t.deepEquals(
+    result,
+    [0, 1, 2, 3, 4],
+    'should take all elements'
+  )
+})
+
+test('iterama: takeAsync + negative more than needed', async (t) => {
+  const iterable = rangeAsync(5)
+  const result = await pipe(
+    takeAsync(-42),
+    toArrayAsync
+  )(iterable)
+
+  t.deepEquals(
+    result,
+    [0, 1, 2, 3, 4],
+    'should take all elements'
+  )
+})
