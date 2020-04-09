a72f292d4b4ff29b0bbc7fdeab0355e4957f5394
Kir Belevich <kir@belevi.ch>
2020-04-09 11:00:20 +0200

    🌱 iterama: add `skipAsync` helper

---
+++ packages/iterama/test/skip-async.ts
@@ -0,0 +1,61 @@
+import test from 'tape'
+import { pipe } from '@psxcode/compose'
+import { skipAsync } from '../src/skip-async'
+import { toArrayAsync } from '../src/to-array-async'
+import { rangeAsync } from '../src/range-async'
+
+test('iterama: skipAsync first', async (t) => {
+  const iterable = rangeAsync(5)
+  const result = await pipe(
+    skipAsync(2),
+    toArrayAsync
+  )(iterable)
+
+  t.deepEquals(
+    result,
+    [2, 3, 4],
+    'should skip first iterations'
+  )
+})
+
+test('iterama: skipAsync more than needed', async (t) => {
+  const iterable = rangeAsync(5)
+  const result = await pipe(
+    skipAsync(42),
+    toArrayAsync
+  )(iterable)
+
+  t.deepEquals(
+    result,
+    [],
+    'should skip everything'
+  )
+})
+
+test('iterama: skipAsync last', async (t) => {
+  const iterable = rangeAsync(5)
+  const result = await pipe(
+    skipAsync(-2),
+    toArrayAsync
+  )(iterable)
+
+  t.deepEquals(
+    result,
+    [0, 1, 2],
+    'should skip last iterations'
+  )
+})
+
+test('iterama: skipAsync last more than needed', async (t) => {
+  const iterable = rangeAsync(5)
+  const result = await pipe(
+    skipAsync(-42),
+    toArrayAsync
+  )(iterable)
+
+  t.deepEquals(
+    result,
+    [],
+    'should skip everything'
+  )
+})
