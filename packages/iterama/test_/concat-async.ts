9451c17012a07f97907a56f8a1bc9e29937f585f
Kir Belevich <kir@belevi.ch>
2021-04-01 16:50:12 +0200

    🚧

---
+++ packages/iterama/test_/concat-async.ts
@@ -0,0 +1,34 @@
+export const tests = [
+  async () => {
+    const { deepStrictEqual } = await import('assert')
+    const { concatAsync } = await import('../src/concat-async')
+    const { toArrayAsync } = await import('../src/to-array-async')
+
+    const iterable1 = {
+      async *[Symbol.asyncIterator]() {
+        for (let i = 1; i <= 5; i++) {
+          yield await Promise.resolve(i)
+        }
+      },
+    }
+    const iterable2 = {
+      async *[Symbol.asyncIterator]() {
+        for (let i = 6; i <= 9; i++) {
+          yield await Promise.resolve(i)
+        }
+      },
+    }
+    const iterables = {
+      *[Symbol.iterator]() {
+        yield iterable1
+        yield iterable2
+      },
+    }
+    const result = await toArrayAsync(concatAsync(iterables))
+
+    deepStrictEqual(
+      result,
+      [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    )
+  },
+]
--- tasks/test.ts
+++ tasks/test.ts
@@ -1,77 +1,107 @@
+import type { CoverageMapData } from 'istanbul-lib-coverage'
+import type { ReportOptions } from 'istanbul-reports'
 import type { TPlugin, TSourceMap, TTask } from './types'
 
-const testIt = (): TPlugin<string, string> => async function* (it) {
+type TReporterName = keyof ReportOptions
+
+const testIt = (): TPlugin<string, CoverageMapData> => async function* (it) {
   const { fileURLToPath } = await import('url')
-  const { drainAsync, mapAsync } = await import('iterama')
+  const { pipe } = await import('funcom')
+  const { drainAsync, mapAsync, ungroupAsync } = await import('iterama')
   const { piAll } = await import('piall')
   const { CoverageInstrumenter } = await import('collect-v8-coverage')
   const { default: v8toIstanbul } = await import('v8-to-istanbul')
-  const { default: { createCoverageMap } } = await import('istanbul-lib-coverage')
-  const { default: { createContext } } = await import('istanbul-lib-report')
-  const reports = await import('istanbul-reports')
   const { fromSource: getSourceMapFromSource } = await import('convert-source-map')
   const { sourcesKey } = await import('@start/ts-esm-loader')
 
   const instrumenter = new CoverageInstrumenter()
-  const coverageMap = createCoverageMap()
   const piAlled = piAll(8)
-
-  await instrumenter.startInstrumenting()
-
   const globl = global as any
 
-  globl[sourcesKey] = {}
+  yield* pipe(
+    mapAsync(async (testFilePath: string) => {
+      await instrumenter.startInstrumenting()
 
-  yield* mapAsync(async (filePath: string) => {
-    const { tests } = await import(`${filePath}?nocache`)
+      globl[sourcesKey] = {}
 
-    try {
-      await drainAsync(piAlled(tests))
-    } catch (err) {
-      console.error(filePath)
-      console.error(err.message)
-    }
+      const { tests } = await import(`${testFilePath}?nocache`)
 
-    return filePath
-  })(it)
+      try {
+        await drainAsync(piAlled(tests))
+      } catch (err) {
+        console.error(testFilePath)
+        console.error(err.message)
+      }
 
-  const coverages = await instrumenter.stopInstrumenting()
+      const v8Coverages = await instrumenter.stopInstrumenting()
+      const istanbulCoverages: CoverageMapData[] = []
 
-  for (const coverage of coverages) {
-    if (coverage.url.includes('iterama/src/concat.ts')) {
-      const filePath = fileURLToPath(coverage.url)
-      const source = globl[sourcesKey][filePath] as string
-      const sourcemap = getSourceMapFromSource(source)!.toObject() as TSourceMap
-      const converter = v8toIstanbul(filePath, 0, { source, sourceMap: { sourcemap } })
+      for (const coverage of v8Coverages) {
+        if (!coverage.url.startsWith('file:///')) {
+          continue
+        }
 
-      await converter.load()
+        const sourceFilePath = fileURLToPath(coverage.url)
 
-      converter.applyCoverage(coverage.functions)
-      coverageMap.merge(converter.toIstanbul())
-    }
-  }
+        if (sourceFilePath !== testFilePath.replace('/test_/', '/src/')) {
+          continue
+        }
 
-  // never happened
-  delete globl[sourcesKey]
+        const source = globl[sourcesKey][sourceFilePath] as string
+        const sourcemap = getSourceMapFromSource(source)!.toObject() as TSourceMap
+        const converter = v8toIstanbul(sourceFilePath, 0, { source, sourceMap: { sourcemap } })
 
-  const context = createContext({ dir: 'coverage/', coverageMap })
-  const report = reports.create('html')
+        await converter.load()
 
-  // @ts-ignore
-  report.execute(context)
+        converter.applyCoverage(coverage.functions)
+        istanbulCoverages.push(converter.toIstanbul())
+      }
+
+      // never happened
+      delete globl[sourcesKey]
+
+      return istanbulCoverages
+    }),
+    ungroupAsync
+  )(it)
+}
+
+const reportIt = (reporterNames: TReporterName[]): TPlugin<CoverageMapData, CoverageMapData> => async function* (it) {
+  const { pipe } = await import('funcom')
+  const { forEachAsync, finallyAsync } = await import('iterama')
+  const { default: { createCoverageMap } } = await import('istanbul-lib-coverage')
+  const { createContext: createReportContext } = await import('istanbul-lib-report')
+  const { create: createReporter } = await import('istanbul-reports')
+
+  const coverageMap = createCoverageMap()
+
+  yield* pipe(
+    forEachAsync((coverageMapData: CoverageMapData) => {
+      coverageMap.merge(coverageMapData)
+    }),
+    finallyAsync(() => {
+      const context = createReportContext({ dir: 'coverage/', coverageMap })
+
+      for (const reporterName of reporterNames) {
+        // @ts-expect-error
+        createReporter(reporterName).execute(context)
+      }
+    })
+  )(it)
 }
 
 export const test: TTask<string, any> = async function* (pkg = 'iterama') {
   const { pipe } = await import('funcom')
   const { find } = await import('./find')
   const { remove } = await import('./remove')
-  // const { mapThreadPool } = await import('@start/thread-pool')
+  const { mapThreadPool } = await import('@start/thread-pool')
 
   yield* pipe(
     find('coverage/'),
     remove,
     find(`packages/${pkg}/test_/*.{ts,tsx}`),
-    testIt()
-    // mapThreadPool(testIt, null)
+    // testIt(),
+    mapThreadPool(testIt, null),
+    reportIt(['html'])
   )()
 }
