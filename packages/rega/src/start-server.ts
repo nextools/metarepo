b1ab210d7bad9a7fbc10bbaf1bdf5188de74f192
Kir Belevich <kir@belevi.ch>
2021-05-08 19:30:03 +0200

    🚧

--- packages/rega/src/start-server.ts
+++ packages/rega/src/start-server.ts
@@ -1,10 +1,14 @@
 import { once } from 'events'
 import http from 'http'
 import dleet from 'dleet'
+import { isPortFree } from 'portu'
+import { sleep } from 'sleap'
 import { unchunkJson, unchunkString } from 'unchunk'
 import { getFreePort } from './get-free-port'
 import { getSocketPath } from './get-socket-path'
 import type { TDeps, TExecutors, TRegisterServiceOptions, TRegisterServiceResult } from './types'
+
+const RETRY_TIMEOUT = 250
 
 export const startServer = async (): Promise<() => Promise<void>> => {
   const registry = new Map<string, number>()
@@ -15,78 +19,55 @@ export const startServer = async (): Promise<() => Promise<void>> => {
     const { pathname } = new URL(req.url!, 'http://localhost')
 
     try {
-      switch (pathname) {
-        case '/register': {
-          const { name, deps: depNames, fromPort, toPort } = await unchunkJson<TRegisterServiceOptions>(req)
+      if (pathname === '/register') {
+        const { name, deps: depNames, fromPort, toPort } = await unchunkJson<TRegisterServiceOptions>(req)
 
-          if (registry.has(name)) {
-            throw new Error(`Service "${name}" is already registered`)
-          }
+        if (registry.has(name)) {
+          throw new Error(`Service "${name}" is already registered`)
+        }
 
-          const deps: TDeps = {}
+        const deps: TDeps = {}
 
-          // wait for dependencies
-          if (Array.isArray(depNames)) {
-            for (const depName of depNames) {
-              if (registry.has(depName)) {
-                deps[depName] = registry.get(depName)!
-              } else {
-                if (!depsQueue.has(depName)) {
-                  depsQueue.set(depName, new Set())
-                }
-
-                const depPort = await new Promise<number>((resolve, reject) => {
-                  depsQueue.get(depName)!.add([resolve, reject])
-                })
-
-                deps[depName] = depPort
-              }
-            }
-          }
-
-          let port: number
-          let hasError = false
-          let error: any
-
-          try {
-            // wait for the rest of already registering services to finish
-            if (concurrencyQueue.size > 0) {
-              await Promise.all(concurrencyQueue.values())
-            }
-
-            const skipPorts = Array.from(registry.values())
-            const portPromise = getFreePort(fromPort, toPort, skipPorts)
-
-            concurrencyQueue.set(name, portPromise)
-
-            port = await portPromise
-
-            concurrencyQueue.delete(name)
-
-            // register
-            registry.set(name, port)
-          } catch (err) {
-            hasError = true
-            error = err
-
-            throw err
-          } finally {
-            // resolve or reject dependents
-            if (depsQueue.has(name)) {
-              const executors = depsQueue.get(name)!
-
-              for (const [resolver, rejecter] of executors) {
-                if (hasError) {
-                  rejecter(error)
-                } else {
-                  resolver(port!)
-                }
+        // wait for dependencies
+        if (Array.isArray(depNames)) {
+          for (const depName of depNames) {
+            if (registry.has(depName)) {
+              deps[depName] = registry.get(depName)!
+            } else {
+              if (!depsQueue.has(depName)) {
+                depsQueue.set(depName, new Set())
               }
 
-              executors.clear()
-              depsQueue.delete(name)
+              const depPort = await new Promise<number>((resolve, reject) => {
+                depsQueue.get(depName)!.add([resolve, reject])
+              })
+
+              deps[depName] = depPort
             }
           }
+        }
+
+        let port: number
+        let hasError = false
+        let error: any
+
+        try {
+          // wait for the rest of already registering services to finish
+          if (concurrencyQueue.size > 0) {
+            await Promise.all(concurrencyQueue.values())
+          }
+
+          const skipPorts = Array.from(registry.values())
+          const portPromise = getFreePort(fromPort, toPort, skipPorts)
+
+          concurrencyQueue.set(name, portPromise)
+
+          port = await portPromise
+
+          concurrencyQueue.delete(name)
+
+          // register
+          registry.set(name, port)
 
           const result: TRegisterServiceResult = { port }
 
@@ -97,42 +78,64 @@ export const startServer = async (): Promise<() => Promise<void>> => {
           res.writeHead(200, { 'Content-Type': 'application/json' })
           res.end(JSON.stringify(result))
 
-          break
-        }
-
-        case '/unregister': {
-          const name = await unchunkString(req)
-
-          if (!registry.has(name)) {
-            throw new Error(`Service "${name}" has never been registered`)
-          }
-
-          const executors = depsQueue.get(name)!
-
-          // reject dependents
+          // wait for port to become in use
           if (depsQueue.has(name)) {
-            for (const executor of executors) {
-              executor[1](new Error(`Service "${name}" has been unregistered`))
+            let isFree = true
+
+            while (isFree) {
+              await sleep(RETRY_TIMEOUT)
+
+              isFree = await isPortFree(port, 'localhost')
             }
           }
+        } catch (err) {
+          hasError = true
+          error = err
 
-          // release concurrency queue
-          if (concurrencyQueue.has(name)) {
-            concurrencyQueue.delete(name)
+          throw err
+        } finally {
+          // resolve or reject dependents
+          if (depsQueue.has(name)) {
+            const executors = depsQueue.get(name)!
+
+            for (const [resolver, rejecter] of executors) {
+              if (hasError) {
+                rejecter(error)
+              } else {
+                resolver(port!)
+              }
+            }
+
+            executors.clear()
+            depsQueue.delete(name)
           }
+        }
+      } else if (pathname === '/unregister') {
+        const name = await unchunkString(req)
 
-          // unregister
-          registry.delete(name)
-
-          res.writeHead(200)
-          res.end()
-
-          break
+        if (!registry.has(name)) {
+          throw new Error(`Service "${name}" has never been registered`)
         }
 
-        default: {
-          throw new Error(`Unknown pathname "${pathname}"`)
+        const executors = depsQueue.get(name)!
+
+        // reject dependents
+        if (depsQueue.has(name)) {
+          for (const executor of executors) {
+            executor[1](new Error(`Service "${name}" has been unregistered`))
+          }
         }
+
+        // release concurrency queue
+        if (concurrencyQueue.has(name)) {
+          concurrencyQueue.delete(name)
+        }
+
+        // unregister
+        registry.delete(name)
+
+        res.writeHead(200)
+        res.end()
       }
     } catch (err) {
       res.writeHead(500, err.message)
--- packages/rega/test/index.ts
+++ packages/rega/test/index.ts
@@ -1,5 +1,23 @@
+import { once } from 'events'
+import { createServer } from 'net'
+import { sleep } from 'sleap'
 import test from 'tape'
 import { startServer, registerService } from '../src'
+
+const startOnPort = async (port: number): Promise<() => Promise<void>> => {
+  const server = createServer()
+
+  server.unref()
+  server.listen(port, 'localhost')
+
+  await once(server, 'listening')
+
+  return async () => {
+    server.close()
+
+    await once(server, 'close')
+  }
+}
 
 test('rega: concurrency queue', async (t) => {
   const stopServer = await startServer()
@@ -38,31 +56,59 @@ test('rega: deps queue', async (t) => {
   const stopServer = await startServer()
 
   try {
-    const [foo, bar, baz, qux] = await Promise.all([
-      registerService({
-        name: 'foo',
-        fromPort: 3000,
-        toPort: 4000,
-        deps: ['bar'],
-      }),
-      registerService({
-        name: 'bar',
-        fromPort: 3000,
-        toPort: 4000,
-      }),
-      registerService({
-        name: 'baz',
-        fromPort: 3000,
-        toPort: 4000,
-        deps: ['foo', 'bar'],
-      }),
-      registerService({
-        name: 'qux',
-        fromPort: 3000,
-        toPort: 4000,
-        deps: ['foo', 'bar'],
-      }),
-    ])
+    const fooP = registerService({
+      name: 'foo',
+      fromPort: 3000,
+      toPort: 4000,
+      deps: ['bar'],
+    })
+    const barP = registerService({
+      name: 'bar',
+      fromPort: 3000,
+      toPort: 4000,
+    })
+    const bazP = registerService({
+      name: 'baz',
+      fromPort: 3000,
+      toPort: 4000,
+      deps: ['foo', 'bar'],
+    })
+    const quxP = registerService({
+      name: 'qux',
+      fromPort: 3000,
+      toPort: 4000,
+      deps: ['foo', 'bar'],
+    })
+
+    const bar = await barP
+    const fooStartTime = Date.now()
+    let closeBar: () => Promise<void>
+
+    void sleep(50)
+      .then(() => startOnPort(bar.port))
+      .then((close) => {
+        closeBar = close
+      })
+
+    const foo = await fooP
+    const fooTime = Date.now() - fooStartTime
+    const bazQuxStartTime = Date.now()
+    let closeFoo: () => Promise<void>
+
+    void sleep(50)
+      .then(() => startOnPort(foo.port))
+      .then((close) => {
+        closeFoo = close
+      })
+
+    const baz = await bazP
+    const qux = await quxP
+    const bazQuxTime = Date.now() - bazQuxStartTime
+
+    t.true(
+      fooTime >= 250,
+      'should wait for bar service port to become in use'
+    )
 
     t.deepEqual(
       foo,
@@ -76,17 +122,25 @@ test('rega: deps queue', async (t) => {
       'should register bar service'
     )
 
+    t.true(
+      bazQuxTime >= 250,
+      'should wait for foo service port to become in use'
+    )
+
     t.deepEqual(
       baz,
       { port: 3002, deps: { foo: 3001, bar: 3000 } },
-      'shouldwait for deps and register baz service'
+      'should wait for deps and register baz service'
     )
 
     t.deepEqual(
       qux,
       { port: 3003, deps: { foo: 3001, bar: 3000 } },
-      'shouldwait for deps and register qux service'
+      'should wait for deps and register qux service'
     )
+
+    await closeFoo!()
+    await closeBar!()
   } finally {
     await stopServer()
   }
